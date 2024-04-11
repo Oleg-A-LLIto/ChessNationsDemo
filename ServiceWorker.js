c7d5fa364622c99f1d688e6a10eaa7fc765843d7
Oleg-A-LLIto <Fedol1408@gmail.com>
2024-04-12 00:46:36 +0400

    0.8.0 demo

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Bastet Games-ChessNations-0.8.0";
+const contentToCache = [
+    "Build/ChessNations.loader.js",
+    "Build/ChessNations.framework.js.gz",
+    "Build/ChessNations.data.gz",
+    "Build/ChessNations.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
