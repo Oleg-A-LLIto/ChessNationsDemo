73b7a48f2859f25c3ef42d4f5cccbf066df9adfe
Oleg-A-LLIto <Fedol1408@gmail.com>
2024-04-26 04:25:24 +0400

    Optimized the build size

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,8 +2,8 @@ const cacheName = "Bastet Games-ChessNations-0.9.8";
 const contentToCache = [
     "Build/ChessNationsDeployment.loader.js",
     "Build/e371bdabaf75e0ab8efae0c99e1161c1.js.unityweb",
-    "Build/dd8f5623a1f904810098d61ed7f966d5.data.unityweb",
-    "Build/5c109c8b1683d07273f0568b8414e385.wasm.unityweb",
+    "Build/e73c3fd19d8d725f68c969b7b7ba5329.data.unityweb",
+    "Build/bc469e4d13d8f4befc632aa26cfe4138.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
